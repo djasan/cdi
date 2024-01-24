54302b1d6d1ca7c1439f31540ce161ebac041dac
sanson <sansondjamila@orange.fr>
2024-01-24 14:57:05 +0100

    creation livres

---
+++ cdi/src/Entity/Livre.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Auteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Editeur = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(string $Image): static
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->Auteur;
+    }
+
+    public function setAuteur(string $Auteur): static
+    {
+        $this->Auteur = $Auteur;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): static
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getEditeur(): ?string
+    {
+        return $this->Editeur;
+    }
+
+    public function setEditeur(string $Editeur): static
+    {
+        $this->Editeur = $Editeur;
+
+        return $this;
+    }
+}
